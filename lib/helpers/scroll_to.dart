069245884d555dd785b10427ba0decb0d9eb1866
Patryk Sewastianowicz <patryk.sewastianowicz@multiway.pl>
2023-01-30 20:20:18 +0100

    Improve scrolling

--- lib/helpers/scroll_to.dart
+++ lib/helpers/scroll_to.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -10,14 +12,12 @@ void scrollTo(String id) {
   Scroll scrollProvider = Provider.of<Scroll>(targetContext, listen: false);
   ScrollController controller = scrollProvider.controller;
 
-  // if (id == "about_me") {
-  //   controller.animateTo(MediaQuery.of(targetContext).size.height,
-  //       duration: const Duration(milliseconds: 500), curve: Curves.ease);
-  //   return;
-  // }
-  // if (id == "home") {
-  //   controller.animateTo(1, duration: const Duration(milliseconds: 500), curve: Curves.ease);
-  //   return;
-  // }
-  Scrollable.ensureVisible(targetContext, duration: const Duration(milliseconds: 500), alignment: -0.2);
+  final renderBox = targetContext.findRenderObject() as RenderBox?;
+  if (renderBox == null) return;
+  var position = renderBox.localToGlobal(Offset.zero).dy;
+  position -= scrollProvider.padding(MediaQuery.of(targetContext)) + 80;
+  position += controller.offset;
+  position = max(0, position);
+
+  controller.animateTo(position, duration: const Duration(milliseconds: 500), curve: Curves.ease);
 }
